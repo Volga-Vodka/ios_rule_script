2baac7a9fa4cefa7fe1c025c5a2830b51a034e07
斯卡曼德骨科诊所 <109299529+Volga-Vodka@users.noreply.github.com>
2026-09-24 02:28:27 +0800

    Update IP.js

--- script/IP.js
+++ script/IP.js
@@ -1,19 +1,29 @@
 if ($response.statusCode != 200) {
-    $done(null);
+  $done(null); // 注意：JavaScript 区分大小写，原代码中的 Null 建议改为标准的 null
 }
+
 var body = $response.body;
 var obj = JSON.parse(body);
+
+// 第一行显示：国家 (例如: United States)
 var title = obj['country'];
+
+// 第二行显示：城市 + 运营商 (例如: San Bruno Verizon 5G Home)
 var subtitle = obj['city'] + ' ' + obj['isp'];
+
+// 获取 IP（新 API 字段为 ip）
 var ip = obj['ip'];
-var description =
-    "国家" + ":" + obj['country'] +
-    '\n' + "城市" + ":" + obj['city'] +
-    '\n' + "运营商" + ":" + obj['isp'] +
-    '\n' + "数据中心" + ":" + obj['organization'];
+
+// 详细信息面板内容（新 API 字段 org 变为 organization 或 asn_organization）
+var description = "国家:" + obj['country'] + '\n' + 
+                  "城市:" + obj['city'] + '\n' + 
+                  "运营商:" + obj['isp'] + '\n' + 
+                  "数据中心:" + obj['organization'];
+
+// 将结果输出给 Quantumult X 面板
 $done({
     title: title,
     subtitle: subtitle,
     ip: ip,
-    description: description
+    content: description 
 });
