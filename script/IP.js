3ba7a80c55d6d9579a167e9eec8f4016b50a848d
斯卡曼德骨科诊所 <109299529+Volga-Vodka@users.noreply.github.com>
2026-09-24 02:07:56 +0800

    Update IP.js

--- script/IP.js
+++ script/IP.js
@@ -1,17 +1,21 @@
 if ($response.statusCode != 200) {
-  $done(null);
+    $done(null);
 }
-
 var body = $response.body;
 var obj = JSON.parse(body);
-
-// 提取新 JSON 中的字段
-var title = obj['country']; 
+// 对应新的 API 字段
+var title = obj['country'];
 var subtitle = obj['city'] + ' ' + obj['isp'];
-var ip = obj['ip']; // 新 API 获取 IP 的字段名是 'ip'
-var description = "国家:" + obj['country'] + '\n' + 
-                  "城市:" + obj['city'] + '\n' + 
-                  "运营商:" + obj['isp'] + '\n' + 
-                  "数据中心:" + obj['asn_organization']; // 使用 asn_organization 或 organization
-
-$done({title, subtitle, ip, description});
+var ip = obj['ip'];
+var description =
+    "国家:" + obj['country'] +
+    '\n' + "城市:" + obj['city'] +
+    '\n' + "运营商:" + obj['isp'] +
+    '\n' + "数据中心:" + obj['organization'];
+// Quantumult X 节点信息显示
+$done({
+    title: title,
+    subtitle: subtitle,
+    ip: ip,
+    description: description
+});
